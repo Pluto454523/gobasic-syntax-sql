7e20a6823705842e78c6e7929a6a350631849d59
PLUTo <nawin3399@gmail.com>
2023-12-25 23:22:38 +0700

    update comment

--- basic-syntax/src/Function.go
+++ basic-syntax/src/Function.go
@@ -2,37 +2,49 @@ package basicSyntax
 
 func Function() {
 
-	// a, _, b := sum(10, 5) //** การรับค่า return จาก function สามารถรับได้หลาย return
+	// ? การรับค่า return จาก function สามารถรับได้หลาย return
+	// a, _, b := sum(10, 5)
 	// fmt.Printf("a = %#v \nb = %#v", a, b)
 
-	// x := func(a, b int) int { //** Anonymous function
+	// ? Anonymous function
+	// x := func(a, b int) int {
 	// 	return a + b
 	// }
+	// println(x(4, 6))
 	// sum := x(8, 6)
 	// println(sum)
 
-	// cal(func(a, b int) int { //** Lamda function
+	// ? Lamda function Ex.1
+	// cal(func(a, b int) int {
 	// 	return a + b
 	// })
 
-	//** Array parameter แบบที่ 1
+	// ? Lamda function Ex.2
+	// cal(add)
+
+	// ? Lamda function EX.3
+	// cal(sub)
+
+	// ? Array parameter แบบที่ 1
 	ArrayP1 := sumArray([]int{1, 2, 3, 4, 5, 6, 7, 8, 9})
 	println(ArrayP1)
 
-	//** Array parameter แบบที่ 2
+	// ? Array parameter แบบที่ 2
 	ArrayP2 := sumArray2(1, 2, 3, 4, 5, 6, 7, 8, 9)
 	println(ArrayP2)
 }
 
 // ** เมื่อส่ง function ใดๆที่ signature เหมือนกันกับ parameter
-// ** cal จะเรียก function นั้นแล้ว ส่งค่า 10,6 ไป
-// ** และ return ค่าไปที่ตัวแปร sum
+// ** cal จะเรียก function นั้นแล้ว ส่งค่า 10,6 ไป และ return ค่าไปที่ตัวแปร sum
+// ** like-Callback
+// ** (func) int
 func cal(f func(int, int) int) {
 	sum := f(10, 6)
 	println(sum)
 }
 
-// ** signature หรือ function type = ** (int,int) int
+// ** signature หรือ function type =
+// ** (int,int) int
 func add(a, b int) int {
 	return a + b
 }
